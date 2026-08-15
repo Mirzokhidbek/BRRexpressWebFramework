dff8078d845b77eca840996061f114f07ab746a4
Mirzokhidbek <mirodev@sju.ac.kr>
2026-08-16 04:11:27 +0900

    fix files

--- server.js
+++ server.js
@@ -1,31 +1,37 @@
-console.log("Web server is running");
 const express = require("express");
 const app = express();
 const http = require("http");
-const PORT = 3000;
 
-//start code
+// 1: Kirish code
 app.use(express.static("public"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-//session code
-//views code
+// 2: Session code
+
+// 3: Views code
 app.set("views", "views");
 app.set("view engine", "ejs");
 
-//Routing code
-app.get("/", (req, res) => {
-    const products = [
-        { name: 'Widget', description: 'A useful widget', price: 19.99 },
-        { name: 'Gadget', description: 'A handy gadget', price: 29.99 },
-        { name: 'Book', description: 'An interesting read', price: 9.99 }
-    ];
+// 4: Routing code
 
-    res.render("market", { products });
-})
- 
-const server =  http.createServer(app);
-server.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+app.post("/create-item", (req, res) => {
+    console.log(req.body);
+
+    res.redirect("/");
+});
+
+app.get("/", function (req, res) {
+    res.render("plans");
+});
+
+// Server
+const server = http.createServer(app);
+
+let PORT = 3000;
+
+server.listen(PORT, function () {
+    console.log(
+        `The server is running successfully on port: ${PORT}, http://localhost:${PORT}`
+    );
 });
